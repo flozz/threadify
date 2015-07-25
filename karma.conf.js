5593df0fba94a0c4501aa2603ab0d1d2cf598091
Fabien LOISON <flo@flogisoft.com>
2015-07-25 18:00:46 +0200

    Travis: run tests through Karma

---
+++ karma.conf.js
@@ -0,0 +1,17 @@
+module.exports = function(config) {
+    config.set({
+        frameworks: ['jasmine'],
+        files: [
+            'dist/threadify.js',
+            'test/*Spec.js'
+        ],
+        reporters: ['progress'],
+        port: 9876,
+        colors: true,
+        logLevel: config.LOG_INFO,
+        autoWatch: false,
+        browsers: ['Firefox', 'Chrome'],
+        captureTimeout: 60000,
+        singleRun: true
+    });
+};
