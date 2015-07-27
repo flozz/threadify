e957927461250730226fd64cb9ec4d601dbe1ca9
Fabien LOISON <flo@flogisoft.com>
2015-07-27 19:15:34 +0200

    All pending tests implemented

--- test/factorySpec.js
+++ test/factorySpec.js
@@ -1,18 +1,52 @@
 describe("Threadify factory", function () {
 
-    xit("can returns a job factory function", function () {
+    it("can returns a job factory function", function () {
+        var fn = threadify(function () {});
+
+        expect(fn instanceof Function).toBeTruthy();
+        expect(fn.toString()).toMatch(/return\s+new\s+Job/);
     });
 
 });
 
 describe("Job factory", function () {
 
-    xit("can spawns a worker", function () {
-        // TODO
+    it("can spawns a worker", function (done) {
+        var callbacks = {
+            done: function () {
+                expect(callbacks.done).toHaveBeenCalledWith("ok");
+                done();
+            }
+        };
+
+        spyOn(callbacks, "done").and.callThrough();
+
+        var fn = threadify(function () {
+            return "ok";
+        });
+
+        var job = fn();
+
+        job.done = callbacks.done;
     });
 
-    xit("can sends arguments to the worker", function () {
-        // TODO
+    it("can sends arguments to the worker", function (done) {
+        var callbacks = {
+            done: function () {
+                expect(callbacks.done).toHaveBeenCalledWith("foobar");
+                done();
+            }
+        };
+
+        spyOn(callbacks, "done").and.callThrough();
+
+        var fn = threadify(function (a, b) {
+            return a + b;
+        });
+
+        var job = fn("foo", "bar");
+
+        job.done = callbacks.done;
     });
 
 });
--- test/workercodeSpec.js
+++ test/workercodeSpec.js
@@ -1,31 +1,199 @@
 describe("Threadify worker", function () {
 
-    xit("can returns a value", function (done) {
-        // TODO
+    it("can returns a value", function (done) {
+        var callbacks = {
+            done: function () {},
+            failed: function () {},
+            terminated: function () {
+                expect(callbacks.done).toHaveBeenCalledWith("ok");
+                expect(callbacks.failed).not.toHaveBeenCalled();
+                expect(callbacks.terminated).toHaveBeenCalled();
+                done();
+            }
+        };
+
+        spyOn(callbacks, "done");
+        spyOn(callbacks, "failed");
+        spyOn(callbacks, "terminated").and.callThrough();
+
+        var fn = threadify(function (a) {
+            return a;
+        });
+
+        var job = fn("ok");
+
+        job.done = callbacks.done;
+        job.failed = callbacks.failed;
+        job.terminated = callbacks.terminated;
     });
 
-    xit("can returns multiple values", function (done) {
-        // TODO
+    it("can returns multiple values", function (done) {
+        var callbacks = {
+            done: function () {},
+            failed: function () {},
+            terminated: function () {
+                expect(callbacks.done).toHaveBeenCalledWith("foo", "bar");
+                expect(callbacks.failed).not.toHaveBeenCalled();
+                expect(callbacks.terminated).toHaveBeenCalled();
+                done();
+            }
+        };
+
+        spyOn(callbacks, "done");
+        spyOn(callbacks, "failed");
+        spyOn(callbacks, "terminated").and.callThrough();
+
+        var fn = threadify(function (a, b) {
+            this.return(a, b);
+        });
+
+        var job = fn("foo", "bar");
+
+        job.done = callbacks.done;
+        job.failed = callbacks.failed;
+        job.terminated = callbacks.terminated;
     });
 
-    xit("can returns a value (async)", function (done) {
-        // TODO
+    it("can returns a value (async)", function (done) {
+        var callbacks = {
+            done: function () {},
+            failed: function () {},
+            terminated: function () {
+                expect(callbacks.done).toHaveBeenCalledWith("foo", "bar");
+                expect(callbacks.failed).not.toHaveBeenCalled();
+                expect(callbacks.terminated).toHaveBeenCalled();
+                done();
+            }
+        };
+
+        spyOn(callbacks, "done");
+        spyOn(callbacks, "failed");
+        spyOn(callbacks, "terminated").and.callThrough();
+
+        var fn = threadify(function (a, b) {
+            var thread = this;
+            setTimeout(function () {
+                thread.return(a, b);
+            }, 100);
+        });
+
+        var job = fn("foo", "bar");
+
+        job.done = callbacks.done;
+        job.failed = callbacks.failed;
+        job.terminated = callbacks.terminated;
     });
 
-    xit("is not terminated if it does not return nothing", function (done) {
-        // TODO
+    it("is not terminated if it does not return nothing", function (done) {
+        var callbacks = {
+            done: function () {},
+            failed: function () {},
+            terminated: function () {}
+        };
+
+        spyOn(callbacks, "done");
+        spyOn(callbacks, "failed");
+        spyOn(callbacks, "terminated").and.callThrough();
+
+        var fn = threadify(function () {});
+
+        var job = fn();
+
+        job.done = callbacks.done;
+        job.failed = callbacks.failed;
+        job.terminated = callbacks.terminated;
+
+        setTimeout(function () {
+            expect(callbacks.done).not.toHaveBeenCalled();
+            expect(callbacks.failed).not.toHaveBeenCalled();
+            expect(callbacks.terminated).not.toHaveBeenCalled();
+            job.terminate();
+            done();
+        }, 100);
     });
 
-    xit("can terminates itself", function (done) {
-        // TODO
+    it("can terminates itself", function (done) {
+        var callbacks = {
+            done: function () {},
+            failed: function () {},
+            terminated: function () {
+                expect(callbacks.done).not.toHaveBeenCalled();
+                expect(callbacks.failed).not.toHaveBeenCalled();
+                expect(callbacks.terminated).toHaveBeenCalled();
+                done();
+            }
+        };
+
+        spyOn(callbacks, "done");
+        spyOn(callbacks, "failed");
+        spyOn(callbacks, "terminated").and.callThrough();
+
+        var fn = threadify(function () {
+            this.terminate();
+        });
+
+        var job = fn();
+
+        job.done = callbacks.done;
+        job.failed = callbacks.failed;
+        job.terminated = callbacks.terminated;
     });
 
-    xit("can reports non fatal error without terminating", function (done) {
-        // TODO
+    it("can reports non fatal error without terminating", function (done) {
+        var callbacks = {
+            done: function () {},
+            failed: function () {},
+            terminated: function () {}
+        };
+
+        spyOn(callbacks, "done");
+        spyOn(callbacks, "failed");
+        spyOn(callbacks, "terminated").and.callThrough();
+
+        var fn = threadify(function () {
+            this.error("test-error");
+        });
+
+        var job = fn();
+
+        job.done = callbacks.done;
+        job.failed = callbacks.failed;
+        job.terminated = callbacks.terminated;
+
+        setTimeout(function () {
+            expect(callbacks.done).not.toHaveBeenCalled();
+            expect(callbacks.failed).toHaveBeenCalledWith("test-error");
+            expect(callbacks.terminated).not.toHaveBeenCalled();
+            job.terminate();
+            done();
+        }, 100);
     });
 
-    xit("can reports errors thrown in the threadified function (and terminating itself)", function (done) {
-        // TODO
+    it("can reports errors thrown in the threadified function (and terminating itself)", function (done) {
+        var callbacks = {
+            done: function () {},
+            failed: function () {},
+            terminated: function () {
+                expect(callbacks.done).not.toHaveBeenCalled();
+                expect(callbacks.failed).toHaveBeenCalledWith("test-error");
+                expect(callbacks.terminated).toHaveBeenCalled();
+                done();
+            }
+        };
+
+        spyOn(callbacks, "done");
+        spyOn(callbacks, "failed");
+        spyOn(callbacks, "terminated").and.callThrough();
+
+        var fn = threadify(function () {
+            throw "test-error";
+        });
+
+        var job = fn();
+
+        job.done = callbacks.done;
+        job.failed = callbacks.failed;
+        job.terminated = callbacks.terminated;
     });
 
 });
